7dff225b2f1fa410e50fa1b0f74c5dd059222e79
Роман Бондаренко <roman.bondarenko@nikel.com.ua>
2020-04-10 09:53:41 +0300

    1) Added base template 2) Added urls for accounts and api apps

--- social/accounts/views.py
+++ social/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+
+class SignUp(View):
+
+  def get(self, request):
+    pass
+
+  def post(self, request):
+    pass
+
+
+class MyLoginView(LoginView):
+  template_name = 'accounts/login.html'
