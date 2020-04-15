4f931e5b3bf4a618c158057a94b469a83361977e
Роман Бондаренко <roman.bondarenko@nikel.com.ua>
2020-04-15 14:55:39 +0300

    1) Removed all constraints for like and dislike

--- social/post/models.py
+++ social/post/models.py
@@ -39,9 +39,6 @@ class Like(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
 
-    class Meta:
-        unique_together = (("post", "user"),)
-
     def __str__(self):
         return f'{self.user}\'s like to "{self.post.title}"'
 
@@ -57,9 +54,6 @@ class Dislike(models.Model):
         verbose_name="Dislike user",
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
-
-    class Meta:
-        unique_together = (("post", "user"),)
 
     def __str__(self):
         return f'{self.user}\'s dislike to "{self.post.title}"'
--- social/post/views.py
+++ social/post/views.py
@@ -17,26 +17,14 @@ class PostViewSet(ModelViewSet):
     @action(detail=True, methods=["POST"])
     def like(self, request, pk=None):
         post = self.get_object()
-        # remove dislike, if user disliked that post before
-        if post.dislikes.filter(user=request.user).exists():
-            post.dislikes.get(user=request.user).delete()
-        try:
-            post.likes.create(user=request.user)
-        except IntegrityError:
-            # means already liked by this user
-            pass
+        post.likes.create(user=request.user)
         serializer = self.get_serializer(post)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     @action(detail=True, methods=["POST"])
     def dislike(self, request, pk=None):
         post = self.get_object()
-        if post.likes.filter(user=request.user).exists():
-            post.likes.get(user=request.user).delete()
-        try:
-            post.dislikes.create(user=request.user)
-        except IntegrityError:
-            pass
+        post.dislikes.create(user=request.user)
         serializer = self.get_serializer(post)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
